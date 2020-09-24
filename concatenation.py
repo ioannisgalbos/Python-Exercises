e2579d4c7a2a10b39a5094842c0b02325646724b
ioannisgalbos <ioannisgalbos@gmail.com>
2020-09-24 15:26:17 +0200

    commit

---
+++ concatenation.py
@@ -0,0 +1,4 @@
+print("Exercise for merging strings with concatenation")
+string1 = input("Enter first string: ")
+string2 = input("Enter second string: ")
+print(string1+string2)
---
+++ concatenation_v2.py
@@ -0,0 +1,5 @@
+#Using input and + as concatenation operator
+firstname = input("May I have your first name, please? ")
+lastname = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + firstname" " + lastname".")
